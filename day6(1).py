ad4b44564694e4900e28d413e8d9647731c6d0d7
sriram99-star <85795394+sriram99-star@users.noreply.github.com>
2021-06-18 21:37:29 +0530

    Add files via upload

---
+++ day6(1).py
@@ -0,0 +1,6 @@
+a=int(input("Enter integer 1:"))
+b=int(input("ENTER INTEGER 2 :"))
+print("Addition of integers : ",a+b)
+print("Subtraction of integers : ",a-b)
+print("Multiplication of integers : ",a*b)
+print("Division of integers : ",a/b)
---
+++ day6(2).py
@@ -0,0 +1,4 @@
+def covid():
+    a=str(input("Enter Patient Name : "))
+    b=float(input("Enter body temperature : "))
+    b=98
